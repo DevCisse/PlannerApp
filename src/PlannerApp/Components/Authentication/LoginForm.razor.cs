d13500e6a3e935001f6ca1576bf8a860ddb2e4a6
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-10-20 09:24:55 +0100

    Implement login functionality and create login form

---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Components.Authentication
+{
+    public partial class LoginForm
+    {
+
+        [Inject]
+        public HttpClient HttpClient{ get; set; }
+
+
+        private LoginRequest model = new LoginRequest();
+
+
+        public async Task HandleSubmit()
+        {
+             await Task.FromResult("Task done");
+        }
+    }
+}
