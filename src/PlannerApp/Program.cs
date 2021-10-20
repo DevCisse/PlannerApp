d8490196226afda6afa97c235cd04b5619e56eda
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-10-20 06:40:40 +0100

    Initialize the Http message handler

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,14 +12,27 @@ using System.Threading.Tasks;
 
 namespace PlannerApp
 {
-    public class Program
+    public partial class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient("PlannerApp.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net/");
+                //  client.DefaultRequestVersion = new Version()
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+            builder.Services.AddTransient<AuthorizationMessageHandler>();   
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+
+
+
+            builder.Services.AddBlazoredLocalStorage();
 
 
             builder.Services.AddMudServices();
